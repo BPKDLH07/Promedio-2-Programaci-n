e6b4499c8e05e6cc47aea4a21d4e2e5ad197be2f
Daniel Arredondo <daniel7november@gmail.com>
2017-10-30 16:32:26 -0500

    Agregado Script Modelo de Prueba

---
+++ Assets/MVC/Modelo.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Modelo : MonoBehaviour {
+		
+		[SerializeField]
+		private bool[] objetos = new bool[2];
+
+
+		void Start(){
+			objetos[0]=false;
+			objetos[1]=false;
+			
+		}
+		void Update(){
+			Inputs();
+	}
+
+	void Inputs(){
+		if(Input.GetKeyDown(KeyCode.A)){
+			objetos[0]=true;
+		}
+		if(Input.GetKeyDown(KeyCode.S)){
+			objetos[1]=true;
+		}
+
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			objetos[0]=false;
+			objetos[1]=false;
+		}
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Crafting01();
+		}
+				
+	}
+
+
+	void Crafting01(){
+		if(objetos[0]==true&&objetos[1]==true){
+			Debug.Log("CrafteadoEsto");
+		} else
+		{
+			Debug.Log("Esto no Craftea");
+		}
+	}
+}
