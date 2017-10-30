b76d0b0fd39403e69e7071d6cf26b0e3fb7e1c7f
Daniel Arredondo <daniel7november@gmail.com>
2017-10-30 18:03:56 -0500

    Ultimos Cambios

--- Assets/MVC/Imputs.cs
+++ Assets/MVC/Imputs.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Imputs : MonoBehaviour {
 	public Modelo modelo;
+	public Vista vista;
 
 	// Use this for initialization
 	void Start () {
@@ -24,18 +25,22 @@ public class Imputs : MonoBehaviour {
 	void Inputs(){
 		if(Input.GetKeyDown(KeyCode.A)){
 			modelo.objetos[0]=true;
+			vista.herramientas[0].SetActive(true);
 		}
 		if(Input.GetKeyDown(KeyCode.S)){
 			modelo.objetos[1]=true;
+			vista.herramientas[1].SetActive(true);
 		}
 
 		if(Input.GetKeyDown(KeyCode.Escape)){
 		modelo.objetos[0]=false;
 		modelo.objetos[1]=false;
+		vista.herramientas[2].SetActive(false);
 		}		
 	}
 
-	 void OnClick(){
+	 public void OnClick(){
 		 modelo.Crafting01();
+		 vista.herramientas[2].SetActive(true);
 	 }
 }
---
+++ Assets/MVC/Vista.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vista : MonoBehaviour {
+
+	[SerializeField]
+	public GameObject[] herramientas = new GameObject[3];
+	// Use this for initialization
+	void Start () {
+		herramientas[0].SetActive(false);
+		herramientas[1].SetActive(false);
+		herramientas[2].SetActive(false);		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Lo últimos cambios hubieron problemas, el ultimo commit, debido a que la maquina se apagó de repente de Salvador
+		
+	}
+}
