a87a76d3f56024eaf275122e204dfa210e55c812
Daniel Arredondo <daniel7november@gmail.com>
2017-10-30 17:45:50 -0500

    Segunda pulido Modelo

--- Assets/MVC/Modelo.cs
+++ Assets/MVC/Modelo.cs
@@ -6,10 +6,6 @@ public class Modelo : MonoBehaviour {
 		
 		[SerializeField]
 		public bool[] objetos = new bool[2];
-<<<<<<< HEAD
-=======
-
->>>>>>> cae6e10e5016c4679e7d9ac4ddcbbf31887a1201
 
 		void Start(){
 			
